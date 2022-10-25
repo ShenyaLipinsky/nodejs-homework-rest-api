4d903d7c5277a84109c4f42e87f80ca8ec5f8c14
ShenyaLipinsky <shenyalipinskiy@gmail.com>
2022-10-25 20:35:45 +0300

    кусщььше

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,69 +2,11 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
-// const createError = require("http-errors");
 
-const { User, schemas } = require("../../models/user/user");
+const { User } = require("../../models/user/user");
 const { auth, upload } = require("../../middlewares");
-// const { sendMail } = require("../../helpers");
 
 const router = express.Router();
-
-// router.get("/verify/:verificationToken", async (req, res, next) => {
-//   try {
-//     const { verificationToken } = req.params;
-//     const user = await User.findOne({ verificationToken });
-//     if (!user) {
-//       throw createError(404);
-//     }
-//     await User.findByIdAndUpdate(user._id, {
-//       verify: true,
-//       verificationToken: "",
-//     });
-//     res.json({
-//       message: "Verification successful",
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// router.post("/verify", async (req, res, next) => {
-//   try {
-//     const { error } = schemas.verify.validate(req.body);
-//     if (error) {
-//       throw createError(400, "missing required field email");
-//     }
-//     const { email } = req.body;
-//     const user = await User.findOne({ email });
-//     if (user.verify) {
-//       throw createError(400, "Verification has already been passed");
-//     }
-//     const mail = {
-//       to: email,
-//       subject: "Подтвеждение email",
-//       html: `<a target="_blank" href='http://localhost:3000/api/users/${user.verificationToken}'>Нажмите чтобы подтвердить свой email</a>`,
-//     };
-//     sendMail(mail);
-//     res.json({
-//       message: "Verification email sent",
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// router.get("/current", auth, async (req, res, next) => {
-//   res.json({
-//     email: req.user.email,
-//   });
-// });
-
-// router.get("/logout", auth, async (req, res, next) => {
-//   const { _id } = req.user;
-//   await User.findByIdAndUpdate(_id, { token: "" });
-//   res.status(204).send();
-// });
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
