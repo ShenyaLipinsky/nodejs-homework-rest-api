5af66db4b164be421ee9c8b7777b04910e5f6106
ShenyaLipinsky <shenyalipinskiy@gmail.com>
2022-10-28 07:51:12 +0300

    remake controller

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -2,10 +2,17 @@ const fs = require("fs/promises");
 const path = require("path");
 const Jimp = require("jimp");
 const { User } = require("../../models/user/user");
+const { RequestError } = require("../../helpers");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
+  if (!req.file) {
+    throw RequestError(
+      400,
+      "Invalid file extension, allow only jpeg, png, img."
+    );
+  }
   try {
     const { _id } = req.user;
 
@@ -15,17 +22,17 @@ const updateAvatar = async (req, res) => {
 
     const filename = `${_id}.${extension}`;
 
-    await Jimp.read(filename)
+    const resultUpload = path.join(avatarsDir, filename);
+
+    await fs.rename(tempUpload, resultUpload);
+
+    await Jimp.read(resultUpload)
       .then((file) => {
-        return file.resize(250, 250).quality(60).greyscale().write(filename);
+        return file.resize(250, 250).quality(60).write(resultUpload);
       })
       .catch((err) => {
         console.error(err);
       });
-
-    const resultUpload = path.join(avatarsDir, filename);
-
-    await fs.rename(tempUpload, resultUpload);
 
     const avatarURL = path.join("avatars", filename);
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,45 +1,17 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs/promises");
-const Jimp = require("jimp");
 
-const { User, schemas } = require("../../models/user/user");
+const { schemas } = require("../../models/user/user");
 const { auth, upload, validation } = require("../../middlewares");
 const { ctrlWrapper } = require("../../helpers");
 const ctrl = require("../../controllers/auth");
 
 const router = express.Router();
 
-const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
-
 router.patch(
   "/avatars",
   auth,
   upload.single("avatar"),
-  async (req, res, next) => {
-    const { _id } = req.user;
-    const { path: tempUpload, filename } = req.file;
-    try {
-      const [extension] = filename.split(".").reverse();
-      const newFileName = `${_id}.${extension}`;
-      const resultUpload = path.join(avatarsDir, newFileName);
-      await fs.rename(tempUpload, resultUpload);
-      await Jimp.read(resultUpload)
-        .then((file) => {
-          return file.resize(250, 250).quality(60).write(resultUpload);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-      const avatarURL = path.join("avatars", newFileName);
-      await User.findByIdAndUpdate(_id, { avatarURL });
-      res.json({
-        avatarURL,
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
+  ctrlWrapper(ctrl.updateAvatar)
 );
 router.post(
   "/register",
